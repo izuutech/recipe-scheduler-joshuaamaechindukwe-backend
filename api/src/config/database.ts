3606f24b8367d223cbe5eefb65b3ac2d484668bd
Izutechs <izutechsofficial@gmail.com>
2025-05-23 00:14:36 +0100

    Remove existing api folder to add as submodule

--- api/src/config/database.ts
+++
@@ -1,7 +0,0 @@
-import config from "./knexfile";
-import { knex } from "knex";
-
-// Initialize Knex instance
-const db = knex(config);
-
-export default db;
--- api/src/schemas/device.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const DeviceSchema = z.object({
-  userId: z.string().min(1),
-  pushToken: z.string().min(1),
-});
